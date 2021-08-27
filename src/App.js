0006a9a1453d58f47a6ef0a8d344618aad038fc5
Jesse-Leegwater <Jleegwater@live.com>
2021-08-26 19:36:53 -0700

    added in the login axios call, also added PrivateRoute but there isnt anything to use it yet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-import Login from './components/Login.js'
+import "./App.css";
+import Login from "./components/Login.js";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  return (
-    <Login />
-  )
+  return <Login />;
 }
 
 export default App;
